14c5cff4aacab262456d8cc5288017fb4711f22f
Pedro WITZEL <pedro.witzel@alstomgroup.com>
2023-08-18 12:50:55 +0200

    Git - enhance git support

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -21,11 +21,26 @@ function _prompt_whoami -a sep_color -a color -d "Display user@host if on a SSH 
   end
 end
 
+function _git_dir
+  echo (command git rev-parse --git-dir 2> /dev/null)
+end
+
 function _git_branch_name
   echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
 end
 
+function _git_tag_name
+  echo (command git describe --tags --exact-match 2> /dev/null)
+end
+
+function _git_commit_sha1
+  echo (command git rev-parse --short HEAD 2> /dev/null)
+end
+
 function _is_git_dirty
+  if test "$theme_display_git_dirty" = no
+    return 0
+  end
   echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
 end
 
@@ -34,11 +49,50 @@ function _git_ahead_count -a remote -a branch_name
     grep '^commit' | wc -l | tr -d ' ')
 end
 
+function _git_read_file -a file_to_read
+  echo (test -f "$file_to_read" && head -n 1 "$file_to_read" | string trim --right)
+end
+
+# Based on git's /mingw64/share/git/completion/git-prompt.sh
+function _git_is_rebasing -a g red
+  set -l r ""
+  set -l b ""
+  set -l step ""
+  set -l total ""
+
+  if test -d "$g/rebase-merge"
+    set b (_git_read_file "$g/rebase-merge/head-name")
+    set step (_git_read_file "$g/rebase-merge/msgnum")
+    set total (_git_read_file "$g/rebase-merge/end")
+    set r "REBASE"
+  else if test -d "$g/rebase-apply"
+    set step (_git_read_file "$g/rebase-apply/next")
+    set total (_git_read_file "$g/rebase-apply/last")
+    if test -f "$g/rebase-apply/rebasing"
+      set b (_git_read_file "$g/rebase-apply/head-name")
+      set r "REBASE"
+    else if test -f "$g/rebase-apply/applying"
+      set r "AM"
+    else
+      set r "AM/REBASE"
+    end
+  else if test -f "$g/MERGE_HEAD"
+    set r "MERGING"
+  else if test -f "$g/BISECT_LOG"
+    set r "BISECTING"
+  end
+
+  if test -n "$r"
+    set b (echo $b | string replace 'refs/heads/' '')
+    echo -n -s $b $red " (" $r " " $step "/" $total ")"
+  end
+end
+
 function _git_dirty_remotes -a remote_color -a ahead_color
   set current_branch (command git rev-parse --abbrev-ref HEAD 2> /dev/null)
   set current_ref (command git rev-parse HEAD 2> /dev/null)
 
-  for remote in (git remote | grep 'origin\|upstream')
+  for remote in (git remote)
 
     set -l git_ahead_count (_git_ahead_count $remote $current_branch)
 
@@ -62,18 +116,35 @@ end
 
 function _prompt_git -a gray normal orange red yellow
   test "$theme_display_git" = no; and return
-  set -l git_branch (_git_branch_name)
-  test -z $git_branch; and return
-  if test "$theme_display_git_dirty" = no
-    echo -n -s $gray '‹' $yellow $git_branch $gray '› '
-    return
+
+  set -l git_dir (_git_dir)
+  test -z "$git_dir"; and return
+
+  set -l git_reference (_git_is_rebasing $git_dir $red)
+  set -l dirty_remotes ""
+
+  if test -z $git_reference
+    set -l git_branch (_git_branch_name)
+    if test "$git_branch" = ''
+      # Check for tag
+      set git_tag (_git_tag_name)
+
+      # if still empty, go for the commit SHA1
+      if test "$git_tag" = ''
+        set git_reference \( (_git_commit_sha1) \)
+      else
+        set git_reference \( $git_tag \)
+      end
+    else
+      set git_reference $git_branch
+      set dirty_remotes (_git_dirty_remotes $red $orange)
+    end
   end
-  set dirty_remotes (_git_dirty_remotes $red $orange)
+
   if [ (_is_git_dirty) ]
-    # TODO - check for MERGING, CHERRY-PICK and REBASE status
-    echo -n -s $gray '‹' $yellow $git_branch $red '*' $dirty_remotes $gray '› '
+    echo -n -s $gray $yellow $git_reference $red '*' $dirty_remotes $gray
   else
-    echo -n -s $gray '‹' $yellow $git_branch $red $dirty_remotes $gray '› '
+    echo -n -s $gray $yellow $git_reference $red $dirty_remotes $gray
   end
 end
 
