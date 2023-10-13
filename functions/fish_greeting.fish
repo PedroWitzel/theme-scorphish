2982b2b6d41622f648914ebd254077c342d8b23d
pedr.o <pedro.witzel@gmail.com>
2023-10-13 12:45:54 +0200

    Clean up and enhance git support (#2)
    * Welcome - remove uname and uptime
    
    * Remove unused
    
    * simplify rust version print
    
    * Git - enhance git support
    
    * prompt order enhance
    
    * Add support for cherry-picking and reverting, and avoid checking for dirty remotes (not efficient)
    
    ---------
    
    Co-authored-by: Pedro WITZEL <pedro.witzel@alstomgroup.com>

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -9,7 +9,5 @@
 
 function fish_greeting -d "what's up, fish?"
   set_color $fish_color_autosuggestion[1]
-  uname -npsr
-  uptime
   set_color normal
 end
--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -6,52 +6,13 @@
 # https://opensource.org/licenses/MIT
 # Copyright (c) 2014, Pablo S. Blum de Aguiar <scorphus@gmail.com>
 
-
-function _prompt_rubies -a color -d 'Display current Ruby (rvm/rbenv)'
-  type -q ruby; or return
-  [ "$theme_display_ruby" = 'no' ]; and return
-  if type -q rvm-prompt
-    if [ "$RUBY_VERSION" != "$LAST_RUBY_VERSION" -o -z "$ruby_version" ]
-      set -gx ruby_version (rvm-prompt i v g)
-      set -gx LAST_RUBY_VERSION $RUBY_VERSION
-    end
-  else if type -q rbenv
-    set -gx ruby_version (rbenv version-name)
-  else if [ -z "$ruby_version" ]
-    set -gx ruby_version (ruby --version | cut -d\  -f2)
-  end
-  echo -n -s $color (echo -n -s $ruby_version | cut -d- -f2-)
-end
-
-function _prompt_virtualenv -a color -d "Display currently activated Python virtual environment"
-  type -q python; or return
-  [ "$theme_display_virtualenv" = 'no' ]; and return
-  if [ "$VIRTUAL_ENV" != "$LAST_VIRTUAL_ENV" -o -z "$PYTHON_VERSION" ]
-    set -gx PYTHON_VERSION (python --version 2>&1 | cut -d\  -f2)
-    set -gx LAST_VIRTUAL_ENV $VIRTUAL_ENV
-  end
-  echo -n -s $color $PYTHON_VERSION
-  [ -n "$VIRTUAL_ENV" ]; and echo -n -s '@'(basename "$VIRTUAL_ENV")
-end
-
 function _prompt_rust -a color -d "Display currently activated Rust"
   type -q rustc; or return
   [ "$theme_display_rust" != 'yes' ]; and return
   if echo $history[1] | grep -q 'rustup default'; or not set -q RUST_VERSION
     set -U RUST_VERSION (rustc --version | cut -d\  -f2)
   end
-  echo -n -s $color $RUST_VERSION
-end
-
-function _prompt_node -a color -d "Display currently activated Node"
-  [ "$theme_display_node" != 'yes' ]; and return
-  type -q node; or return
-  type -q nvm; and begin; set -q NVM_BIN; or return; end # Lazy loading
-  if [ "$NVM_BIN" != "$LAST_NVM_BIN" -o -z "$NODE_VERSION" ]
-    set -gx NODE_VERSION (node --version)
-    set -gx LAST_NVM_BIN $NVM_BIN
-  end
-  [ -n "$NODE_VERSION" ]; and echo -n -s $color $NODE_VERSION
+  echo -n -s "|" 🦀 $color $RUST_VERSION
 end
 
 function _prompt_whoami -a sep_color -a color -d "Display user@host if on a SSH session"
@@ -60,12 +21,27 @@ function _prompt_whoami -a sep_color -a color -d "Display user@host if on a SSH 
   end
 end
 
+function _git_dir
+  echo (command git rev-parse --git-dir 2> /dev/null)
+end
+
 function _git_branch_name
   echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
 end
 
+function _git_tag_name
+  echo (command git describe --tags --exact-match 2> /dev/null)
+end
+
+function _git_commit_sha1
+  echo (command git rev-parse --short HEAD 2> /dev/null)
+end
+
 function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
+  if test "$theme_display_git_dirty" = no
+    return 0
+  end
+  echo (command git status --short --ignore-submodules=all 2> /dev/null)
 end
 
 function _git_ahead_count -a remote -a branch_name
@@ -73,11 +49,58 @@ function _git_ahead_count -a remote -a branch_name
     grep '^commit' | wc -l | tr -d ' ')
 end
 
+function _git_read_file -a file_to_read
+  echo (test -f "$file_to_read" && head -n 1 "$file_to_read" | string trim --right)
+end
+
+# Based on git's /mingw64/share/git/completion/git-prompt.sh
+function _git_is_rebasing -a g red
+  set -l r ""
+  set -l b ""
+  set -l step ""
+  set -l total ""
+
+  if test -d "$g/rebase-merge"
+    set b (_git_read_file "$g/rebase-merge/head-name")
+    set step (_git_read_file "$g/rebase-merge/msgnum")
+    set total (_git_read_file "$g/rebase-merge/end")
+    set r "REBASE"
+  else if test -d "$g/rebase-apply"
+    set step (_git_read_file "$g/rebase-apply/next")
+    set total (_git_read_file "$g/rebase-apply/last")
+    if test -f "$g/rebase-apply/rebasing"
+      set b (_git_read_file "$g/rebase-apply/head-name")
+      set r "REBASE"
+    else if test -f "$g/rebase-apply/applying"
+      set r "AM"
+    else
+      set r "AM/REBASE"
+    end
+  else if test -f "$g/REVERT_HEAD"
+    set r "REVERTING"
+  else if test -f "$g/CHERRY_PICK_HEAD"
+    set r "CHERRY-PICKING"
+  else if test -f "$g/MERGE_HEAD"
+    set r "MERGING"
+  else if test -f "$g/BISECT_LOG"
+    set r "BISECTING"
+  end
+
+  if test -n "$r"
+    if test -n "$b"
+      set b (echo $b | string replace 'refs/heads/' '')
+      echo -n -s $b $red " (" $r " " $step "/" $total ")"
+    else
+      echo -n -s $b $red " (" $r ")"
+    end
+  end
+end
+
 function _git_dirty_remotes -a remote_color -a ahead_color
   set current_branch (command git rev-parse --abbrev-ref HEAD 2> /dev/null)
   set current_ref (command git rev-parse HEAD 2> /dev/null)
 
-  for remote in (git remote | grep 'origin\|upstream')
+  for remote in (git remote)
 
     set -l git_ahead_count (_git_ahead_count $remote $current_branch)
 
@@ -95,30 +118,39 @@ function _git_dirty_remotes -a remote_color -a ahead_color
 end
 
 function _prompt_versions -a blue gray green orange red append
-  set -l prompt_rubies (_prompt_rubies $red)
-
-  set -l prompt_virtualenv (_prompt_virtualenv $blue)
-
   set -l prompt_rust (_prompt_rust $orange)
-
-  set -l prompt_node (_prompt_node $green)
-
-  echo -n -e -s "$prompt_rubies $prompt_virtualenv $prompt_rust $prompt_node" | string trim | string replace -ar " +" "$gray|" | tr -d '\n'
+  echo -n -e -s "$prompt_rust"
 end
 
 function _prompt_git -a gray normal orange red yellow
   test "$theme_display_git" = no; and return
-  set -l git_branch (_git_branch_name)
-  test -z $git_branch; and return
-  if test "$theme_display_git_dirty" = no
-    echo -n -s $gray '‹' $yellow $git_branch $gray '› '
-    return
+
+  set -l git_dir (_git_dir)
+  test -z "$git_dir"; and return
+
+  set -l git_reference (_git_is_rebasing $git_dir $red)
+
+  if test -z $git_reference
+    set -l git_branch (_git_branch_name)
+    if test "$git_branch" = ''
+      # Check for tag
+      set git_tag (_git_tag_name)
+
+      # if still empty, go for the commit SHA1
+      if test "$git_tag" = ''
+        set git_reference \( (_git_commit_sha1) \)
+      else
+        set git_reference \( $git_tag \)
+      end
+    else
+      set git_reference $git_branch
+    end
   end
-  set dirty_remotes (_git_dirty_remotes $red $orange)
+
   if [ (_is_git_dirty) ]
-    echo -n -s $gray '‹' $yellow $git_branch $red '*' $dirty_remotes $gray '› '
+    echo -n -s $gray $yellow $git_reference $red '*' $gray ' '
   else
-    echo -n -s $gray '‹' $yellow $git_branch $red $dirty_remotes $gray '› '
+    echo -n -s $gray $yellow $git_reference $red $gray ' '
   end
 end
 
@@ -151,37 +183,31 @@ function fish_prompt
   set -l orange (set_color ff9900)
   set -l green (set_color green)
 
-  printf $gray'['
+  printf '\n'$gray'['
 
   _prompt_whoami $gray $green
 
   if test "$theme_display_pwd_on_second_line" != yes
     _prompt_pwd
-    printf '%s|' $gray
+    printf '%s' $gray
   end
 
   _prompt_versions $blue $gray $green $orange $red
 
-  printf '%s] ⚡️ %0.3fs' $gray (math $CMD_DURATION / 1000)
-
-  if set -q SCORPHISH_GIT_INFO_ON_FIRST_LINE
-    set theme_display_git_on_first_line
-  end
+  printf '%s]'
 
   if set -q theme_display_git_on_first_line
+    printf ' '
     _prompt_git $gray $normal $orange $red $yellow
   end
 
-  if test "$theme_display_pwd_on_second_line" = yes
-    printf $gray'\n‹'
-    _prompt_pwd
-    printf $gray'›'
-  end
+  printf '⚡️ %0.3fs' (math $CMD_DURATION / 1000)
 
   printf '\n'
   if not set -q theme_display_git_on_first_line
     _prompt_git $gray $normal $orange $red $yellow
   end
+
   _prompt_status_arrows $exit_code
   printf ' '
 
